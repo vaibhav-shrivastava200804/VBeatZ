6a99bb832153b399e554539e61bbbc042b1d81e5
vaibhav-shrivastava200804 <vaibhav2000804@gmail.com>
2024-09-16 11:07:20 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -100,7 +100,8 @@ async function playMusic(track) {
 
 async function displayAlbums() {
   console.log("albums");
-  let a = await fetch("/Songs/");
+  let a = await fetch(`/Songs/`);
+     console.log("hello");
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
